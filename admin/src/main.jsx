af5d409e7e4b8b99935b12ea49b84c92e18210ab
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-12-23 13:14:35 -0300

    Criando a pasta admin

---
+++ admin/src/main.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Adicione sua Clerk Publishable Key ao arquivo .env')
+}
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  </ClerkProvider>
+)
